4eb6c6f0c1445b9cd55d00ba9297b777e99f531b
evan0416 <cycloevan97@gmail.com>
2024-05-23 16:19:27 +0900

    cods: update code

--- mini-project/main.py
+++ mini-project/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from model import Todo
 
 app = FastAPI()
 
@@ -7,4 +8,47 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+todos = []
 
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+
+# Get signle todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "No todos found"}
+
+
+
+# Create a todo
+@app.post("/todos")
+async def get_todo(todo: Todo):
+    todos.append(todo)
+    return {"message" : "Todo has been added"}
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def updat_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"todo": todo}
+    return {"message": "No todos found to update"}
+
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"todo": "Todo has been DELETED!"}
+    return {"message": "No todos found"}
